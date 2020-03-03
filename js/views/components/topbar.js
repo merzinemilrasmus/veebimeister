d2ac139924b179cba87b2b33a649618120b579c4
rasmusmerzin <rasmusmerzin@gmail.com>
2020-03-03 15:41:33 +0200

    navbar search

--- js/views/components/topbar.js
+++ js/views/components/topbar.js
@@ -1,14 +1,15 @@
 import elem from '/js/libs/elem.js';
 
-const barHeight = '3rem';
+const barHeight = 2.5;
 const links = [ 'avaleht', 'koolitused', 'kontakt', 'login' ];
 
 export default () => {
   const div = elem('div', {
     style: {
       width: '100%',
-      height: barHeight,
-      padding: '.5rem',
+      boxSizing: 'border-box',
+      height: `${barHeight +1.6}rem`,
+      padding: '.8rem',
       position: 'fixed',
       top: 0,
       left: 0,
@@ -16,14 +17,16 @@ export default () => {
       color: 'var(--white)',
       gridGap: '1rem',
       display: 'grid',
-      gridTemplateColumns: `repeat(${links.length +1}, auto)`,
+      gridTemplateColumns: `repeat(${links.length +1}, auto) 1fr`,
       justifyContent: 'start',
     }
   });
+
   const logo = elem('img', {
     style: { height: '100%' },
     src: '/img/logo.svg',
   }, div);
+
   links.forEach(link => {
     const hAvaleht = elem('a', {
       href: `/${link}`,
@@ -31,10 +34,48 @@ export default () => {
       style: {
         textTransform: 'capitalize',
         display: 'block',
-        lineHeight: barHeight,
+        lineHeight: `${barHeight}rem`,
         verticalAlign: 'middle',
       }
     }, div);
   });
+
+  const search = elem('form', {
+    style: {
+      display: 'grid',
+      gridGap: '.6rem',
+      gridTemplateColumns: 'auto auto',
+      justifyContent: 'end',
+      alignItems: 'center',
+    }
+  }, div);
+
+  const searchBar = elem('input', {
+    type: 'text',
+    placeholder: 'otsi',
+    style: {
+      height: '1rem',
+      padding: '.5rem',
+      border: 'none',
+      borderRadius: `${barHeight}rem`,
+    }
+  }, search);
+
+  const searchButton = elem('button', {
+    style: {
+      background: '#0000',
+      border: 'none',
+      height: '1.8rem',
+    },
+    onclick: () => location.assign(`/koolitused/${searchBar.value}`)
+  }, search);
+
+  const searchIcon = elem('img', {
+    src: '/img/search-outline-white.svg.png',
+    style: {
+      height: '100%',
+    }
+  }, searchButton);
+
   document.body.appendChild(div);
 }
