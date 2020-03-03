08c1625c1bb72afe112632cac06db2f3b973a793
rasmusmerzin <rasmusmerzin@gmail.com>
2020-03-03 15:08:27 +0200

    navbar

--- js/views/components/topbar.js
+++ js/views/components/topbar.js
@@ -1,14 +1,40 @@
 import elem from '/js/libs/elem.js';
 
+const barHeight = '3rem';
+const links = [ 'avaleht', 'koolitused', 'kontakt', 'login' ];
+
 export default () => {
-  elem('div', {
-    innerHTML: 'navbar',
+  const div = elem('div', {
     style: {
       width: '100%',
+      height: barHeight,
+      padding: '.5rem',
       position: 'fixed',
-      background: '#0002',
       top: 0,
       left: 0,
+      background: '#0006',
+      color: 'var(--white)',
+      gridGap: '1rem',
+      display: 'grid',
+      gridTemplateColumns: `repeat(${links.length +1}, auto)`,
+      justifyContent: 'start',
     }
-  }, document.body);
+  });
+  const logo = elem('img', {
+    style: { height: '100%' },
+    src: '/img/logo.svg',
+  }, div);
+  links.forEach(link => {
+    const hAvaleht = elem('a', {
+      href: `/${link}`,
+      innerHTML: link,
+      style: {
+        textTransform: 'capitalize',
+        display: 'block',
+        lineHeight: barHeight,
+        verticalAlign: 'middle',
+      }
+    }, div);
+  });
+  document.body.appendChild(div);
 }
