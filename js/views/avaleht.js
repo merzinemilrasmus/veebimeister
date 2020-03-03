d6158560f475226a2a48733e237c07f09b91cca6
markoleht <54833627+markoleht@users.noreply.github.com>
2020-03-03 14:01:46 +0200

    gitignore

--- js/views/avaleht.js
+++ js/views/avaleht.js
@@ -4,7 +4,6 @@ import topbar from './components/topbar.js';
 
 export default () => {
   topbar();
-  const title = elem('h1', {
-    innerHTML: 'Chat'
-  }, document.body);
+  const title = elem('h1', { innerHTML: 'Chat' }, document.body);
+  const button = elem('button', { innerHTML: 'testButton'}, title);
 };
