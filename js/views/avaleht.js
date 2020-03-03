ffe22f2e8c3567cc6c8cb2b1087f7d5b1bd287af
markoleht <54833627+markoleht@users.noreply.github.com>
2020-03-03 14:25:02 +0200

    file structure; images

--- js/views/avaleht.js
+++ js/views/avaleht.js
@@ -4,6 +4,22 @@ import topbar from './components/topbar.js';
 
 export default () => {
   topbar();
-  const title = elem('h1', { innerHTML: 'Chat' }, document.body);
-  const button = elem('button', { innerHTML: 'testButton'}, title);
+  const wallpaper = elem('img', {
+    style: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      zIndex: -1
+    }
+  }, document.body);
+  wallpaper.src = '/img/pilt.jpg';
+  const company = elem('p', {
+    innerHTML:"Terav pliiats",
+    style: {
+      position: 'absolute',
+      right: '10vw',
+      top: '40vh'
+    }
+  }, document.body);
 };
