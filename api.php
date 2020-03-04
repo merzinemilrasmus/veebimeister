2e428dbfae087983151c6b63cef25ec82588a56a
rasmusmerzin <rasmusmerzin@gmail.com>
2020-03-04 09:36:03 +0200

    database++

--- api.php
+++ api.php
@@ -8,19 +8,22 @@ $pdo = new PDO("mysql:dbname=$dbName;host=$dbHost", DB_USER, DB_PASS);
 
 $pdo->exec('
 	create table if not exists KOOLITUS (
-		koolituseId int auto_increment key,
-		koolituseNimi varchar(63) not null,
-		koolituseSisu text not null,
+		id int auto_increment key,
+		aeg datetime not null,
+		nimi varchar(63) not null,
+		sisu text not null,
 		koolitaja varchar(63) not null,
 		asukoht varchar(63) not null,
 		hind int not null,
-		kohtadearv int not null
+		kohtadeArv int not null
 	);
 	create table if not exists OSALEJA (
 		osalejaId int auto_increment key,
 		nimi varchar(63) not null,
 		tel varchar(31) not null,
-		email varchar(63) not null
+		email varchar(63) not null,
+		sessionId char(32),
+		sessionTime datetime
 	);
 	create table if not exists OSALEMINE (
 		id int auto_increment key,
@@ -28,6 +31,8 @@ $pdo->exec('
 		koolituseId int not null
 	);
 ');
+
+
 
 switch ($_SERVER['REQUEST_METHOD']) {
 	case 'POST':
