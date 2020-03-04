d032b31f11ca647692b8e046defce9bdffcfb9ad
ikt <ikt@khk.ee>
2020-03-04 09:06:08 +0200

    json api base

--- api.php
+++ api.php
@@ -2,4 +2,17 @@
 
 require_once './php/config.php';
 
+$dbHost = DB_HOST;
+$dbName = DB_NAME;
+$pdo = new PDO("mysql:dbname=$dbName;host=$dbHost", DB_USER, DB_PASS);
+
+switch ($_SERVER['REQUEST_METHOD']) {
+	case 'POST':
+		$input = (array) json_decode(file_get_contents('php://input'));
+		$output = $input;
+		echo json_encode($output);
+		break;
+	default: header('Location: /');
+}
+
 ?>
